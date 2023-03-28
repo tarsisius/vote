89f9df6fb3b36caef104ea561dc98e4bf61769d6
Tarsisius Hayo <trsharyo@gmail.com>
2023-03-28 19:55:07 +0700

    fixing bugs

--- app/event/[unique]/page.tsx
+++ app/event/[unique]/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = 'force-dynamic'
 
 export default async function Event({ params }: any) {
   const { unique } = params
-  const res = await fetch(`https://vote.thp.my.id/api/get-event/${unique}`, {
+  const res = await fetch(`${process.env.APP_URL}/api/get-event/${unique}`, {
     cache: 'no-cache',
   })
   if (!res.ok) {
